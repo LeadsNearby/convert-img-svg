134a02108e1b362b16f0f29cf87d81dc924d99ce
Andrew Gillingham <andrew@leadsnearby.com>
2017-08-08 12:55:03 -0400

    Add jQuery as dependency for register script

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ License: GPLv2 or later
 
 // Register and insert javascript
 function lnb_insert_svg_script() {
-	wp_register_script('convert-img-svg', plugins_url('/convert-img-svg/script.min.js'), '', '', 'true');
+	wp_register_script('convert-img-svg', plugins_url('/convert-img-svg/script.min.js'), array('jquery'), '', 'true');
 	wp_enqueue_script('convert-img-svg');
 }
 add_action('wp_enqueue_scripts', 'lnb_insert_svg_script');	
